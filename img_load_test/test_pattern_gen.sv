2abe45ee27e07b552ca11cae819e4aff4255a924
Aniruddh <aniruddh2002@vt.edu>
2025-04-21 23:35:02 -0400

    updated Chess baord picture and test_pattern_gen.sv

--- img_load_test/test_pattern_gen.sv
+++ img_load_test/test_pattern_gen.sv
@@ -51,18 +51,17 @@ module test_pattern_gen #(
 
     logic [5:0] rel_x;
     logic [5:0] rel_y;
-	assign tile_col = (hcount - BOARD_ORIGIN_X) / CHESS_TILE_SIZE;
+    assign tile_col = (hcount - BOARD_ORIGIN_X) / CHESS_TILE_SIZE;
     assign tile_row = (vcount - BOARD_ORIGIN_Y) / CHESS_TILE_SIZE;
 
-	 assign rel_x = hcount - (BOARD_ORIGIN_X + tile_col * CHESS_TILE_SIZE);
-	 assign rel_y = vcount - (BOARD_ORIGIN_Y + tile_row * CHESS_TILE_SIZE);
-	 
+    assign rel_x = hcount - (BOARD_ORIGIN_X + tile_col * CHESS_TILE_SIZE);
+    assign rel_y = vcount - (BOARD_ORIGIN_Y + tile_row * CHESS_TILE_SIZE);
 
     logic [3:0] piece_index;
     logic in_sprite_area;
 
     // Piece layout (12 sprites in ROM): index from 0–11
-    logic [3:0] piece_map[8][8];
+    logic [3:0] piece_map[7:0][7:0];
 
     initial begin
         piece_map[0] = '{0, 1, 2, 3, 4, 2, 1, 0};
@@ -76,13 +75,13 @@ module test_pattern_gen #(
     end
 
     // --- ROMs
-    title_screen_rom2 title_rom_inst (
+    title_screen_rom title_rom_inst (
         .address(rom_address),
         .clock(vga_clk),
         .q(title_pixel)
     );
 
-    player_screen_rom2 player_rom_inst (
+    player_screen_rom player_rom_inst (
         .address(rom_address),
         .clock(vga_clk),
         .q(player_pixel)
@@ -94,7 +93,7 @@ module test_pattern_gen #(
         .q(board_pixel)
     );
 
-    chess_piece_rom2 pieces_rom (
+    chess_piece_rom pieces_rom (
         .address(piece_rom_addr),
         .clock(vga_clk),
         .q(piece_pixel)
@@ -102,28 +101,32 @@ module test_pattern_gen #(
 
     // --- Main pixel generation
     always_comb begin
-		piece_rom_addr = 0;
-		selected_pixel  = board_pixel;
-		piece_index     = 0;
-		in_sprite_area  = 0;
-		
+        piece_rom_addr = 0;
+        selected_pixel = board_pixel;
+        piece_index    = 0;
+        in_sprite_area = 0;
+
         case (SW[1:0])
             2'b00: selected_pixel = title_pixel;
             2'b01: selected_pixel = player_pixel;
             2'b10: selected_pixel = board_pixel;
             2'b11: begin
-                selected_pixel = board_pixel;
                 if (hcount >= BOARD_ORIGIN_X && hcount < BOARD_ORIGIN_X + 8 * CHESS_TILE_SIZE &&
                     vcount >= BOARD_ORIGIN_Y && vcount < BOARD_ORIGIN_Y + 8 * CHESS_TILE_SIZE) begin
 
                     piece_index = piece_map[tile_row][tile_col];
                     in_sprite_area = (piece_index < 12);
 
-                    if (in_sprite_area) begin
-                        piece_rom_addr = piece_index * CHESS_PIECE_SIZE * CHESS_PIECE_SIZE + rel_y * CHESS_PIECE_SIZE + rel_x;
+                    if (in_sprite_area && rel_y >= 16) begin
+                        piece_rom_addr = piece_index * CHESS_PIECE_SIZE * CHESS_PIECE_SIZE +
+                                         (rel_y - 16) * CHESS_PIECE_SIZE + rel_x;
                         selected_pixel = (piece_pixel != 0) ? piece_pixel : board_pixel;
-                    end 
-                end 
+                    end else begin
+                        selected_pixel = board_pixel;
+                    end
+                end else begin
+                    selected_pixel = board_pixel;
+                end
             end
             default: selected_pixel = board_pixel;
         endcase
@@ -137,7 +140,6 @@ module test_pattern_gen #(
     };
 
 endmodule
-
 
 
 
